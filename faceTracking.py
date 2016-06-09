5758967295bbbfc51050ee606177de56836400c9
Devin Wieker <devin.wieker@gmail.com>
2016-06-09 13:12:36 -0700

    enabled image resizing

--- faceTracking.py
+++ faceTracking.py
@@ -1,29 +1,32 @@
 import cv2
 import numpy as np
-from imutils.video import WebcamVideoStream
-from imutils.video import VideoStream
-from imutils.video import FPS
-import imutils
 import time
-from scipy.misc import imresize
+from scipy.misc import imresize, imrotate
 
 # Initialize Cascade Calssifiers
-faceCascade = cv2.CascadeClassifier("/usr/local/Cellar/opencv/2.4.13/share/OpenCV/haarcascades/haarcascade_frontalface_default.xml")
-eyeCascade = cv2.CascadeClassifier("/usr/local/Cellar/opencv/2.4.13/share/OpenCV/haarcascades/haarcascade_eye.xml")
+faceCascade = cv2.CascadeClassifier("haarcascades/haarcascade_frontalface_default.xml")
+eyeCascade = cv2.CascadeClassifier("haarcascades/haarcascade_eye.xml")
 
+t = time.time()
 
-vid_name = "me"
-ROI_scale = .3
+vid_name = "people.mp4"
+ROI_padding = .2
+ROI_speed = .10
+consec_facelss_frames = 0
+max_faceless_frames = 5
+jerk_threshold = 0
+face_y_cut = .3; face_x_cut = .2
+eye_n = 0; eye_scaling = 1.3 
+face_n = 1; face_scaling = 1.1 
+img_size = 1. #Shrink by this factor 
+
 
 # Initialize Web Cam Thread
-#vs = VideoStream(src="/Users/devinwieker/Desktop/Metis/faceTrack/videos/me/" + vid_name + ".mov").start()
 #vs = cv2.VideoCapture(0)
-vs = cv2.VideoCapture("/Users/devinwieker/Desktop/Metis/faceTrack/videos/me/" + vid_name + ".mp4")
-#vs = cv2.VideoCapture("/Users/devinwieker/Desktop/Metis/faceTrack/videos/me/people.mp4")
-fps = FPS().start()
+vs = cv2.VideoCapture("videos/" + vid_name)
 
 
-# Face fixels to be saved for processing
+# Face pixels to be saved for processing
 pixels = []
 
 # Read one frame to initialize ROI dimensions
@@ -31,150 +34,209 @@ ret, frame = vs.read()
 
 print frame.shape
 
-
 ROI = ((0, 0), (frame.shape[1], frame.shape[0]))
+prev_face = None
 
 while(True):
     # Capture frame
-    ret, frame = vs.read()
+    ret, frame_original = vs.read()
 
     if frame is None:
     	break
 
-    #frame = imresize(frame, .2)
+  
+    #frame = imrotate(frame, -90)
+    frame = frame_original.copy()
+    frame = imresize(frame, img_size)
 
     # Line Plot
     plot = np.ones((200, 400, 3))
 
-    # Face tracking works on gray scale images
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-
-    # Region of interest. 
+    # Cut out region of interest to reduce computational time. 
     (x1, y1), (x2, y2) = ROI
-    gray = gray[y1: y2, x1 : x2]
+    gray = cv2.cvtColor(frame[y1:y2, x1:x2], cv2.COLOR_BGR2GRAY)
 
 
-    # Equalize histogram
-    #gray = cv2.equalizeHist(gray)
-    
-    # # Draw ROI rectangle
-    # cv2.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
-
-    # # Detect faces in the gray image
-    # faces = faceCascade.detectMultiScale(
-    #     gray,
-    #     scaleFactor=2.0,
-    #     minNeighbors=2,
-    #     minSize=(30, 30),
-    #     flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-    # )
+    # Detect faces in the gray image
+    faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=face_scaling,
+        minNeighbors=face_n,
+        minSize=(30, 30),
+        flags = cv2.cv.CV_HAAR_SCALE_IMAGE
+    )
 
 
-    # # Initially assume no face was found. Now, search through canidate faces
-    # face_found = False
-    # for (x, y, w, h) in faces:
+    # Initially assume no face was found. Now, search through canidate faces
+    face_found = False
+    for (x, y, w, h) in faces:
 
-    # 	#Draw rect around candidate face
-    #     cv2.rectangle(frame, (x1 + x, y1 + y), (x1+x+w, y1+y+h), (0, 255, 255), 4)
+    	#Draw rect around candidate face
+        #cv2.rectangle(frame, (x1 + x, y1 + y), (x1+x+w, y1+y+h), (0, 255, 255), 4)
         
-    #     # Check for eyes! If no eyes, no face!
-    #     eyes = eyeCascade.detectMultiScale(
-    #         gray[y:y+h, x:x+w],
-    #         minNeighbors=1,
-    #         minSize=(int(w*.1), int(h*.1)), #Eyes should not be tiny compared to face
-    #     )
+        # Check for eyes! If no eyes, no face!
+        eyes = eyeCascade.detectMultiScale(
+            gray[y:y+h, x:x+w],
+            scaleFactor=eye_scaling,
+            minNeighbors=eye_n,
+            minSize=(int(w*.1), int(h*.1)), #Eyes should not be tiny compared to face
+        )
         
-    #     # Were Eyes found?
-    #     if len(eyes) > 0: 
+        # Shift back x and y to original reference frame
+        x += x1; y += y1
+
+        # Were Eyes found?
+        if len(eyes) > 0: 
+
+            # Reset faceless frame count
+            consec_facelss_frames = 0
+
+            # Calculate New Region of interest. Speeds up computation
+            # ROI is an enlarged box around the face
+            x1_new, y1_new, x2_new, y2_new =(max(0, x - int(ROI_padding*w)),
+                                             max(0, y - int(ROI_padding*h)),
+                                             min(frame.shape[1], x + int((1+ROI_padding)*w)),
+                                             min(frame.shape[0], y + int((1+ROI_padding)*h)))
+        
+            # If there was a face last frame, just use that position again
+            # UNLESS significant movement occured
+            if prev_face:
+                (x_old, y_old, w_old, h_old) = prev_face
+                
+                # Check if new face shifted significantly from the old
+                xdiff = abs((x_old + w + x_old)/2. -  (x2_new + x1_new)/2.)
+                ydiff = abs((y_old + h + y_old)/2. -  (y2_new + y1_new)/2.)
+
+                # If only slight movement, keep face from previous frame
+                small_diff = (xdiff < jerk_threshold*w_old) and (ydiff < jerk_threshold*h_old)
+                if small_diff:
+                    x,y,w,h = prev_face
+                    print "Using previous frame face position"
+
+                # If significant movement, update ROI and use new face
+                else:
+                    print "prev face exists, but cur face has significantly moved"
+                    print "prev face: " , prev_face
+                    print "cur face: " , (x,y,w,h)
+                    print "ROI: " , ROI
+                    
+                    ROI = ((x1_new, y1_new), (x2_new, y2_new))
+                    prev_face=(x,y,w,h)
             
-    #         for eye in eyes:
-    #             (xe, ye, we, he) = eye
-    #             cv2.rectangle(frame, (x1+x+xe, y1+y+ye), (x1+xe+x+we, y1+ye+y+he), (0, 255, 0), 2)
+            # No previous face; we MUST use this new face
+            else:
+                ROI = ((x1_new, y1_new), (x2_new, y2_new))
+                prev_face=(x,y,w,h)
+                print "No prev face, using new face"
+                print "cur face:" , (x,y,w,h)
+                print "ROI:" , ROI
 
-
-    #     	# Draw Face bounding box
-    #         cv2.rectangle(frame, (x1+x, y1+y), (x1+x+w, y1+y+h), (0, 255, 0), 2)
-                 
-           
-    #         # Grab middle of face and append mean pixels
-    #         face = frame[y1 + y + int(.3*h): y1 + y + h - int(.3*h), x1 + x + int(.2*w): x1 + x + w - int(.2*w)]
-    #         pixels.append([face[:, :,  0].mean(), 
-    #                        face[:, :,  1].mean(), 
-    #                        face[:, :,  2].mean()
-    #                      ])
-
-    #         cv2.imshow("face", imresize(face, (300,300)))
             
-    #         # Display line plot
-    #         if len(pixels) >= 30:
-    #             numpy_pixels = np.array(pixels[-30:])
-    #             r, g, b = numpy_pixels[:,0], numpy_pixels[:,1], numpy_pixels[:,2]
-    #             r = (r - r.mean()) / r.std()
-    #             g = (g - g.mean()) / g.std()
-    #             b = (b - b.mean()) / b.std()
-    #             p = (r[0], g[0], b[0])
-    #             x_scale = 400 / len(g)
-    #             y_offset =  100
-    #             for i, point in enumerate(zip(r,g,b)):
-    #                 cv2.line(plot, ((i-1)*x_scale, y_offset + int(p[0]*30)), 
-    #                          (i*x_scale, y_offset + int(point[0]*30)), 
-    #                          (255, 0, 0), 2)
+            # # Display line plot
+            # if len(pixels) >= 30:
+            #     numpy_pixels = np.array(pixels[-30:])
+            #     r, g, b = numpy_pixels[:,0], numpy_pixels[:,1], numpy_pixels[:,2]
+            #     r = (r - r.mean()) / r.std()
+            #     g = (g - g.mean()) / g.std()
+            #     b = (b - b.mean()) / b.std()
+            #     p = (r[0], g[0], b[0])
+            #     x_scale = 400 / len(g)
+            #     y_offset =  100
+            #     for i, point in enumerate(zip(r,g,b)):
+            #         cv2.line(plot, ((i-1)*x_scale, y_offset + int(p[0]*30)), 
+            #                  (i*x_scale, y_offset + int(point[0]*30)), 
+            #                  (255, 0, 0), 2)
 
-    #                 cv2.line(plot, ((i-1)*x_scale, y_offset + int(p[1]*30)), 
-    #                          (i*x_scale, y_offset + int(point[1]*30)), 
-    #                          (0, 255, 0), 2)
+            #         cv2.line(plot, ((i-1)*x_scale, y_offset + int(p[1]*30)), 
+            #                  (i*x_scale, y_offset + int(point[1]*30)), 
+            #                  (0, 255, 0), 2)
 
-    #                 cv2.line(plot, ((i-1)*x_scale, y_offset + int(p[2]*30)), 
-    #                          (i*x_scale, y_offset + int(point[2]*30)), 
-    #                          (0, 0, 255), 2)
+            #         cv2.line(plot, ((i-1)*x_scale, y_offset + int(p[2]*30)), 
+            #                  (i*x_scale, y_offset + int(point[2]*30)), 
+            #                  (0, 0, 255), 2)
                     
 
-    #                 p = point
+            #         p = point
             
-    #             # Display the resulting frame
-    #             cv2.imshow('plot', plot) 
+            #     # Display the resulting frame
+            #     cv2.imshow('plot', plot) 
 
-                 
-    #         # Update Region of interest. Speeds up computation
-    #         x1_new, y1_new, x2_new, y2_new = (max(0, x1 + x - int(ROI_scale*w)),
-    #                                          max(0, y1 + y - int(ROI_scale*h)),
-    #                                          min(frame.shape[1], x1 + x + int((1+ROI_scale)*w)),
-    #                                          min(frame.shape[0], y1 + y + int((1+ROI_scale)*h)))
-             
-    #         ROI = ((x1_new, y1_new), (x2_new, y2_new))
-            
-               
-    #         face_found = True
-    #         break
+            y = int(y/img_size); x = int(x/img_size); h = int(h/img_size); w = int(w/img_size)       
+            # Grab middle of face and append mean pixels
+            face = frame_original[y + int(face_y_cut*h): y + h - int(face_y_cut*h), 
+                         x + int(face_x_cut*w): x + w - int(face_x_cut*w)]
+            pixels.append([face[:, :,  0].mean(), 
+                           face[:, :,  1].mean(), 
+                           face[:, :,  2].mean()
+                         ])
+
+            cv2.imshow("face", imresize(face, (300,300)))
+
+            # Draw Face rectangle!
+            cv2.rectangle(frame_original, (x, y), (x+w, y+h), (0, 255, 0), 2)
+            midx = (x + w + x)/2; midy = (y+h+y)/2
+            cv2.circle(frame, (midx,midy), 5, color=(0,0,0)) 
+
+            # Draw rects around eyes 
+            for eye in eyes:
+                (xe, ye, we, he) = eye
+                ye = int(ye/img_size); xe = int(xe/img_size); he = int(he/img_size); w = int(we/img_size)         
+                cv2.rectangle(frame_original, (x+xe, y+ye), (xe+x+we, ye+y+he), (0, 255, 0), 1)
+
+                         
+            face_found = True
+            break
     
-    # # If no face found, expand the ROI to full screen
-    # if not face_found:
-    #     ROI = ((0, 0), (frame.shape[1], frame.shape[0]))
+    # If no face was found, expand the ROI - may have moved too fast
+    if not face_found:
+        ROI_w, ROI_h = x2-x1, y2-y1
+        ROI =((max(0, x1 - int(ROI_speed*ROI_w)), 
+               max(0, y1 - int(ROI_speed*ROI_h))),
 
+              (min(frame.shape[1], x2  + int(ROI_speed*ROI_w)), 
+               min(frame.shape[0], y2+int(ROI_speed*ROI_h)))) 
 
-    # font = cv2.FONT_HERSHEY_SIMPLEX
-    # cv2.putText(frame, str(fps.fps()),(0,30), font, 1, (0,0,0),2)
+        consec_facelss_frames += 1 
 
-    # # Display the resulting frame
-    # cv2.imshow('main', frame)  
+        if consec_facelss_frames > max_faceless_frames:
+            prev_face = None      
 
-    print fps.fps()
+        if prev_face:
+            x,y,w,h = prev_face 
+                         
+
+            y = int(y/img_size); x = int(x/img_size); h = int(h/img_size); w = int(w/img_size)         
+            # Grab middle of face and append mean pixels
+            face = frame_original[y + int(face_y_cut*h): y + h - int(face_y_cut*h), 
+                         x + int(face_x_cut*w): x + w - int(face_x_cut*w)]
+            pixels.append([face[:, :,  0].mean(), 
+                           face[:, :,  1].mean(), 
+                           face[:, :,  2].mean()
+                         ])
+
+            #cv2.imshow("face", imresize(face, (300,300)))
+
+            cv2.rectangle(frame_original, (x, y), (x+w, y+h), (0, 255, 255), 2)
+            midx = (x + w + x)/2; midy = (y+h+y)/2
+            cv2.circle(frame_original, (midx,midy), 5, color=(0,0,0))
     
-    fps.update()
-            
+    
+
+    font = cv2.FONT_HERSHEY_SIMPLEX
+    cv2.putText(frame_original, "%.2f"%(1 / (time.time() - t)),(0,30), font, 1, (0,0,0),2)
+    
+    # Display the resulting frame
+    cv2.imshow('main', frame_original)  
+
+    t = time.time()
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# do a bit of cleanup
-fps.stop()  
 
-#np.savetxt(vid_name + ".txt", np.array(pixels))  
+np.savetxt("data/" + vid_name.split(".")[0] + ".txt", np.array(pixels))  
 
-print fps.fps()
-print vs.stream.get(cv2.cv.CV_CAP_PROP_FPS)
+print vs.get(cv2.cv.CV_CAP_PROP_FPS)
 
 cv2.destroyAllWindows()
-#vs.stop()
 vs.release()
 
